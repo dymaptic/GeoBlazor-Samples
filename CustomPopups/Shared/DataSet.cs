9eff7c4493314d04a6a22f277c657876a1ade794
Christopher Moravec <christopher@dymaptic.com>
2022-10-19 17:06:59 -0700

    CustomPopups VersionBump to 1.1.1

--- CustomPopups/Shared/DataSet.cs
+++ CustomPopups/Shared/DataSet.cs
@@ -6,6 +6,15 @@ public class DataSet
     public double? Longitude;
     public string? Name;
     public string? Description;
+    public string? Address;
+
+    public Dictionary<string, object> Attributes =>
+        new Dictionary<string, object>()
+        {
+            {nameof(Name), Name},
+            {nameof(Description), Description},
+            {nameof(Address), Address}
+        };
 
     public static List<DataSet> GenerateSomePoints(double centerLat, double centerLong)
     {
@@ -17,7 +26,8 @@ public class DataSet
                 Latitude = centerLat + Random.Shared.NextDouble(),
                 Longitude = centerLong + Random.Shared.NextDouble(),
                 Name = $"This is a Name {i}",
-                Description = $"This is a description {i}"
+                Description = $"This is a description {i}",
+                Address = $"{i}{i}{i} Some Road Name"
             };
             data.Add(r);
         }
