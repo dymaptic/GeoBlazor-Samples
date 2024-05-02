bbf0090a2cc4c396f7f3c96c70301fe31fdeee59
Tim Purdum <tim.purdum@dymaptic.com>
2024-05-02 10:07:31 -0500

    start with empty map

--- MuseumsOfChicago/GeoBlazor.Sample.Museums/Program.cs
+++ MuseumsOfChicago/GeoBlazor.Sample.Museums/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
 builder.Services.AddGeoBlazorPro(builder.Configuration);
 
 builder.Services.AddCors();
