4302e05d0c1364de5dca02335b3e6893231faeea
Tim Purdum <tim.purdum@dymaptic.com>
2025-07-26 10:23:57 -0500

    add simple-search

---
+++ SimpleSearch/SimpleSearch/Program.cs
@@ -0,0 +1,44 @@
+using SimpleSearch.Client.Pages;
+using SimpleSearch.Components;
+using dymaptic.GeoBlazor.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddGeoBlazor(builder.Configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+#if (ENABLE_COMPRESSION)
+app.MapStaticAssets();
+#else
+app.UseStaticFiles();
+#endif
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(SimpleSearch.Client._Imports).Assembly);
+
+app.Run();
